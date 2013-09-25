889f09b31661ef289a32f3db27a6c573be5d87ae
Ivo van der Wijk <github@in.m3r.nl>
2013-09-25 09:29:16 +0200

    working

--- rt.py
+++ rt.py
@@ -4,6 +4,7 @@
 
 from math import sqrt, ceil
 import random
+import sys
 
 G = [247570,280596,280600,249748,18578,18577,231184,16,16]
 
@@ -18,20 +19,25 @@ def Trace(o, d, t, n):
     m = 0
     p = -o.z/d.z
 
+    if .01 < p:
+        t = p
+        n = vector(0, 0, 1)
+        m = 1
+
     for k in range(18, -1, -1):
         for j in range(8, -1, -1):
             if G[j] & 1 << k:
-                p = o.add(vector(-k, 0, -j-4))
-                b = p.dot(d)
-                c = p.dot(p)-1
+                p = o + vector(-k, 0, -j-4)
+                b = p % d
+                c = p % p - 1
                 q = b*b-c
 
                 if q > 0:
                     s = -b-sqrt(q)
 
-                    if s < t and s > 0.1: ## XXX rewrite 
+                    if s < t and s > 0.01: ## XXX rewrite 
                         t = s
-                        n = p.add(d.scale(t)).normalize()
+                        n = -(p+d*t)
                         m = 2
 
     return m, t, n
@@ -45,43 +51,41 @@ def Sample(o, d):
     m, t, n = Trace(o, d, t, n)
 
     if not m:
-        ## pow(1-d.z, 4) XXX
-        return vector(.7,.6, 1).scale(1-d.z ** 4)
+        return vector(.7,.6, 1) * ((1-d.z) ** 4)
 
     # A sphere was maybe hit
-    h = o.add(d).scale(t)
-    l = vector(9+R(), 9+R(), 16).add(h).scale(-1).normalize()
-    ## not sure about % * precedence
-    r = d.add(n.scale(n.dot(d.scale(-2))))
+    h = o+d*t
+    l = -(vector(9+R(), 9+R(), 16) + h * -1)
+    r = d + n * (n%d*-2)
 
     # lambertian factor
-    b = l.dot(n)
+    b = l % n
 
     # illumination factor
     if b < 0 or Trace(h, l, t, n)[0]:
         b = 0
 
     # calculate the color 'p' 
-    p = l.dot(r.scale(1 if b > 0 else 0))**99
+    p = (l % r * (1 if b > 0 else 0))**99
 
-    if m == 1:
-        h = h.scale(.2)
-        if int(ceil(h.x)+ceil(h.y) == 1):
+    if m %2 == 1:
+        h = h * .2
+        if (ceil(h.x)+ceil(h.y)) % 2 == 1:
             x = vector(3, 1, 1)
         else:
-            x = vector(3,3,3)
-        return x.scale(b.scale(.2).add(.1))
+            x = vector(3, 3, 3)
+        return x*(b*.2+.1)
 
     # m == 2 sphere was hit
-    return vector(p, p, p).add(Sample(h, r).scale(.5))
+    return vector(p, p, p) + Sample(h, r) * .5
 
 S = Sample
 
 class vector(object):
     def __init__(self, x=0.0, y=0.0, z=0.0):
-        self.x = x
-        self.y = y
-        self.z = z
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
 
     def add(self, v): # +
         return vector(self.x+v.x, self.y+v.y, self.z+v.z)
@@ -103,37 +107,39 @@ class vector(object):
                       self.z*v.x-self.x*v.z,
                       self.x*v.y-self.y*v.x)
 
-    __ixor__ = cross
+    __xor__ = cross
 
     def normalize(self): # !
-        # print "!", self.x, self.y, self.z
-        return self.scale(1/sqrt(self.dot(self)))
+        return self * (1.0/sqrt(self % self))
    
-    __not__ = normalize
+    __neg__ = normalize
 
     def __str__(self):
-        print "Vector(%f, %f, %f)" % (self.x, self.y, self.z)
+        return "Vector(%f, %f, %f)" % (self.x, self.y, self.z)
 
 def main():
-    print "P6 512 512 255",
-
+    sys.stdout.write("P6 512 512 255 ")
+    
     ## ! has precedence over * ?
-    g = vector(-6, 16, 0).normalize()  # camera direction
-    a = vector(0, 0, 1).cross(g).normalize().scale(0.002) # camera up vec
-    b = g.cross(a).normalize().scale(0.002) # right vector
-    c = a.add(b).scale(-256).add(g) # ??
+    g = -vector(-6, -16, 0)  # camera direction
+    a = -(vector(0,0,1)^g)*.002
+    b = -(g^a) * 0.002
+    c = (a+b)*-256+g
 
+    raycount = 64
+
+    rr,gg,bb = 13, 13, 13
     for y in range(511, -1, -1):
         for x in range(511, -1, -1):
-            p = vector(13, 13, 13)
-            for r in range(63, -1, -1):
-                t = a.scale(R()-.5).scale(99).add(b.scale(R()-.5).scale(99))
+            p = vector(rr, gg, bb)
+            for r in range(raycount-1, -1, -1):
+                t = a*(R()-.5)*99+b*(R()-.5)*99
+                p = Sample(vector(17, 16, 8)+t,
+                           -(t*-1+(a*(R()+x)+b*(y+R())+c)*16)
+                          )*3.5+p
 
-                p = Sample(vector(17, 16, 8).add(t),
-                           t.scale(-1).add(a.scale(R()+x).add(b.scale(y+R()).add(c))).scale(16)).scale(3.5).add(p)
-
-            print p
-            print "%s%s%s" % (chr(int(p.x)), chr(int(p.y)), chr(int(p.z)))
+            sys.stdout.write("%s%s%s" % (chr(int(p.x)), chr(int(p.y)), chr(int(p.z))))
+        sys.stdout.flush()
 
 if __name__ == '__main__':
     main()
